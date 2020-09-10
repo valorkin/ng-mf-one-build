4b060e1b4569cc454499baf2b9da558787cb9397
Dmitriy Shekhovtsov <valorkin@gmail.com>
2020-09-11 00:20:37 +0300

    chore: added fonts

--- projects/shell/src/app/landing/landing.component.ts
+++ projects/shell/src/app/landing/landing.component.ts
@@ -5,6 +5,16 @@ import {PluginOptions} from '../plugins/plugin';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
+  styles: [`
+    .carousel-container {
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      padding: 20px;
+      height: 250px;
+    }
+  `]
 })
 export class LandingComponent implements OnInit {
   prlist: PluginOptions;
