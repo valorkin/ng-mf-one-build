3736d539ae72afa1a4a4be054d3c3f6699e41eba
Dmitriy Shekhovtsov <valorkin@gmail.com>
2020-09-10 23:05:15 +0300

    chore: layout page

--- projects/shell/src/app/landing/landing.component.ts
+++ projects/shell/src/app/landing/landing.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {LookupService} from '../plugins/lookup.service';
+import {PluginOptions} from '../plugins/plugin';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
 })
 export class LandingComponent implements OnInit {
+  prlist: PluginOptions;
+  yourfav: PluginOptions;
+  constructor(
+    private lookupService: LookupService) {
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const plugins = await this.lookupService.lookup();
+    this.prlist = plugins.find(p => p.componentName === 'PrListComponent');
+    this.yourfav = plugins.find(p => p.componentName === 'YourFavoritesComponent');
   }
 
 }
