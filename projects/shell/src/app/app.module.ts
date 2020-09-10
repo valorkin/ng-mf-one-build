378407b92d23108403e852d10a54c3a048321447
Dmitriy Shekhovtsov <valorkin@gmail.com>
2020-09-10 17:48:04 +0300

    chore: WIP routing

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+import {IconModule, LayoutGridModule, LayoutPanelModule, LinkModule} from '@fundamental-ngx/core';
+
 import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.routes';
 import { ConfigComponent } from './config/config.component';
 import { PluginProxyComponent } from './plugins/plugin-proxy.component';
-import {PrListModule} from '../../../mfe1/src/app/pr-list/pr-list.module';
 import {HomeComponent} from './home/home.component';
+import {LandingComponent} from './landing/landing.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-
-    // todo: extract
-    // PrListModule,
-
+    IconModule, LayoutGridModule, LayoutPanelModule, LinkModule,
     RouterModule.forRoot(APP_ROUTES)
   ],
   declarations: [
     AppComponent,
     HomeComponent,
+    LandingComponent,
     PluginProxyComponent,
     ConfigComponent
   ],
--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,16 +1,20 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { loadRemoteModule } from '../federation-utils';
-import { ConfigComponent } from './config/config.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ConfigComponent} from './config/config.component';
+import {LandingComponent} from './landing/landing.component';
 
 export const APP_ROUTES: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
-    {
-      path: 'config',
-      component: ConfigComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'config',
+    component: ConfigComponent
+  },
+  {
+    path: 'layout',
+    component: LandingComponent
+  }
 ];
